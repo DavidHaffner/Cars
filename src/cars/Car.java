ac56fec49433b187f74f459dcb6a1aa992094753
Student <Student@192.168.0.214>
2017-08-15 19:51:08 +0200

    práce z kurzu dne 15/8

---
+++ src/cars/Car.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cars;
+
+/**
+ *
+ * @author Student
+ */
+public class Car {
+    private String manufacture;
+    private String modelName;
+    private int year;
+    private static int counter=0;
+    private int speed;
+    private Engine engine;
+    
+    public Car (String manufacture, String modelName, int year, int speed, String engine) {
+        this.manufacture = manufacture;
+        this.modelName = modelName;
+        this.year = year;
+        counter ++;
+        this.speed = speed;
+        this.engine = new Engine (engine);
+    }
+    
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+    public String getManufacture() {
+        return manufacture;
+    }
+    public String getModelName() {
+        return modelName;
+    }
+    public int getYear() {
+        return year;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public Engine getEngine() {
+        return engine;
+    }
+    
+    protected void finalize () throws Throwable {
+        try {
+            counter --;
+        } finally {
+            super.finalize();
+        }
+    }
+}
